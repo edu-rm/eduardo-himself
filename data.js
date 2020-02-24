c531b8e542e5b0e9cb51836e3193cafef02e188b
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-02-24 00:57:38 -0300

    Feature: event click leads pesquisa page

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
         avatar_img : "https://avatars3.githubusercontent.com/u/37454606?s=460&v=4",
-        title : "T",
+        title : "T ",
         description : "Lorem ipsum dolor q saepe porro architecto incidunt, est mollitia eius!",
         access_link: "https://www.github.com",
         featured:true
     },
     {
         avatar_img : "https://avatars3.githubusercontent.com/u/37454606?s=460&v=4",
-        title : "Título do repositorio",
+        title : "c",
         description : "Lorem ipsum dolor quas saepe porro architecto incidunt, est mollitia eius!",
         access_link: "https://www.github.com",
         featured:true
