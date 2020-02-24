5e5e4c79a9c78127fc1c8457b8e0c6645f2b9646
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-02-23 23:53:43 -0300

    Feature: Add featured wrapper

--- server.js
+++ server.js
@@ -4,29 +4,31 @@ const nunjucks = require("nunjucks")
 
 const server = express()
 const projetos = require("./data.js")
-
+const info = {
+    avatar_url : "https://i.imgur.com/iCmED2B.jpg",
+    name : "Eduardo Rampon Meireles",
+    bio : "Um eterno aprendiz",
+    links : [
+        { name : "GitHub", url : "https://github.com/EduardoRamponMeireles"},
+        { name : "Instagram", url : "https://www.instagram.com/eduardo.rampon/"},
+        { name : "LinkedIn", url : "https://github.com/EduardoRamponMeireles"},
+    ]
+}
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape:false,
+    noCache:true
 })
 
 
 
 server.get("/", function(req, res){
-    const info = {
-        avatar_url : "https://i.imgur.com/iCmED2B.jpg",
-        name : "Eduardo Rampon Meireles",
-        bio : "Um eterno aprendiz",
-        links : [
-            { name : "GitHub", url : "https://github.com/EduardoRamponMeireles"},
-            { name : "Instagram", url : "https://www.instagram.com/eduardo.rampon/"},
-            { name : "LinkedIn", url : "https://github.com/EduardoRamponMeireles"},
-        ]
-    }
+    
 
     return res.render("sobre", { info : info})
 })
